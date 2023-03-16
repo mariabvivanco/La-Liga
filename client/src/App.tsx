7fdf9e7cfe22a2bef46152807a086dedf411af6c
betty <mariab.vivanco@gmail.com>
2023-03-16 17:16:21 +0100

    Cambiando un estilo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme, useColorMode } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
 import store from './store/index';
 import { RoutesApp } from './routes';
 
